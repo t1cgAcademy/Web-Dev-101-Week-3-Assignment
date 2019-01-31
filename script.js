802f83d0c477739b20da0a1e9f05508d52d45406
steve <steve@Steves-MacBook-Pro.local>
2019-01-31 18:23:19 -0500

    Save before adding style comments

--- script.js
+++ script.js
@@ -1,61 +1,50 @@
-const button = document.getElementById("buttonGuess");
-//create a variable called input that gets the id of guess using getElementById
-const secretNumber = Math.floor(Math.random() * 100);
-//create a variable called resetButton that creates a button element using document.createElement()
-let guessCount =  0;
+// Declare button, resetButton, numberOfGuesses variables for DOM manipulation using getElementById
+// (hint: watch your ids)
+// Declare input and prevGuesses variables using query selector to get the id guesss
+// FYI: the function below creates a random non-floating point number between
+// 1 and 100
+let secretNumber = Math.floor(Math.random() * 100);
+// intialize guessCount variable with value of 0
+//  (hint: we should let it be able to change value later)
 
-setGameOver = () => {
-  //Disable the input value field and the button value field below.  This will require some
-  //googling.  An important programming skill.
-  resetButton.textContent = "Try Again";
-  //Use the document.body.appendChild() function to add/append the resetButton to the DOM.
-  //Add an event listener called resetGame to your newly created resetButton using addEventListener();
-  //Remember, it takes two arguments, the function (click or hover for example) and resetGame.
-}
+// Declare setGameOver function {
+// set the disabled attribute of input to true
+// set the disabled attribute of button to be true
+// set the textContent of resetButton to 'Try Again';
+// Toggle the hidden class of resetButton
+// note 'hidden' is a predefined css class, which is why you do not 
+// need to add anything to style.css for this
+// }
 
 resetGame = () => {
-  //reset guessCount to zero
-  //Declare resetResult variable and link to all elements with the class resultField using
-  //querySelectorAll.
-  //Iterate through resetResult and set the textContent of all these fields to an empty string
-  resetButton.parentNode.removeChild(resetButton);
-  input.disabled = false;
-  button.disabled = false;
-  input.value = "";
-  //Set prevGuesses textContent to an empty string.
-  input.focus();
-  const secretNumber = Math.floor(Math.random() * 100);
-}
+  // reset guessCount to 0
+  // reset the text of numberOfGuesses to empty string
+  // toggle hidden class on reset button.  
+  // set disabled property of input to false
+  // set disabled property of button to false
+  // set input value to empty string
+  // set text of prevGuesses to empty string
 
-guess = () => {
-//set the guestCount variable (remember you can because you used "let") increment
-//by one everytime a guess is entered with the guess button
-document.getElementById("numOfGuesses").textContent = guessCount;
-//create lowOrHigh variable for lowOrHigh <p> tag for DOM manipulation using document.querySelector.
-//hint: Do not forget the #
-const stringGuess = Number(input.value);
-//create prevGuesses variable for prevGuesses <p> tag for DOM manipulation using document.querySelector.
-//hint: Do not forget the #
-prevGuesses.textContent += stringGuess + "  ";
-//Remember: Keeping track of values using console.log can be useful
-//console.log the value of guessCount below
-//in the below if statement, set the first condition to compare the stringGuess variable with
-//the secretNumber variable.  Create an alert if stringGuess is === to secretNumber
-//Then call the setGameOver function to reset the game
-if (){
-
-
-} else if (stringGuess > secretNumber && guessCount < 11){
-  lowOrHigh.textContent = "You are too high!!";
-}
-  //Create another else if with the conditions if the stringGuess is less than the secretNumber
-  //and the guessCount is less than 11 then the textContent of lowOrHigh will change to "You are too low!!!"
-  //Similar to the code above
-else if (){
-
-} else {
-  lowOrHigh.textContent = "Game Over!";
-  setGameOver();
-}
-
+  input.focus(); // highlights element when active
+  // set secretNumber to another random number
 };
+
+enterGuess = () => {
+  // add one to guessCount when function is called
+  // set text of numberOfGuess to new guessCount
+  // declare lowOrHi variable using querySelector
+  // declare stringGuess variable set to the input value
+  // hint: stringGuess must be a number for game to work properly
+  prevGuesses.textContent += stringGuess + '  ';
+
+  // use control structure to set lowOrHi guess hint display
+  // you will set three specific conditions...
+  // 1. If the stringGuess is equal to the secretNumber, display
+  // an alert stating 'You got it right!!' and then call setGameOver() function
+  // 2. If stringGuess is greater than the secretNumber and the guessCount
+  // is less than 11, set the text of lowOrHi to 'You are too hi!!'
+  // 3. If the stringGuess is less than the secretNumber and the guessCount
+  // is less than 11, set the text of lowOrHi to 'You are too low!!!'
+  // 4. If anything else happens, set the text of lowOrHi to 'Game Over!'
+  // and then call setGameOver() function.
+}
